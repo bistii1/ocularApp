c65f78947adc99897bf1d516dd2ee514c8b1b7c4
lvanden0 <lily.vandenbergh@icloud.com>
2026-03-04 15:50:44 -0500

    installed more packages

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,26 @@
-import { Text, View } from "react-native";
+import { Text, TextInput, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+      </View>
+      <TextInput
+        placeholder="User ID"
+        style={{
+          height: 40,
+          width: 200,
+          borderWidth: 1,
+          borderColor: 'gray',
+          margin: 10,
+        }}
+      />
+   
   );
 }
