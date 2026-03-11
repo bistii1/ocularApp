a214c77441102d7620c8a6e424edbeefe236ede9
lvanden0 <lily.vandenbergh@icloud.com>
2026-03-10 22:26:20 -0400

    Added switch to choose eye

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,15 @@
-import { Text, TextInput, View } from "react-native";
+import { useState } from "react";
+import { Switch, Text, TextInput, View } from "react-native";
 
 
 export default function Index() {
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+  const leftEyeText = 'Left Eye';
+  const rightEyeText = 'Right Eye';
+
   return (
     <View>
-      <View
-        style={{
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Text>Edit app/index.tsx to edit this screen.</Text>
-      </View>
       <TextInput
         placeholder="User ID"
         style={{
@@ -21,8 +18,19 @@ export default function Index() {
           borderWidth: 1,
           borderColor: 'gray',
           margin: 10,
+          padding: 10,
         }}
       />
+      <Text style={isEnabled ? { color: '#767577' } : { color: 'black' }}>{leftEyeText}</Text>
+      <Switch
+          ios_backgroundColor="#b0afb0"
+          trackColor={{false: '#b0afb0', true: '#b0afb0'}}
+          thumbColor={isEnabled ? '#564bf5' : '#564bf5'}
+          onValueChange={toggleSwitch}
+          value={isEnabled}
+        />
+      <Text style={isEnabled ? { color: 'black' } : { color: '#767577' }}>{rightEyeText}</Text>
    </View>
+   
   );
 }
