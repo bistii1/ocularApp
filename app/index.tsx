3f669b43e52d91a20e11cd00e4679b4bf273371f
lvanden0 <lily.vandenbergh@icloud.com>
2026-03-10 23:16:41 -0400

    added start button and fixed switch layout

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { Switch, Text, TextInput, View } from "react-native";
+import React, { useState } from "react";
+import { Button, StyleSheet, Switch, Text, TextInput, View } from "react-native";
 
 
 export default function Index() {
@@ -7,6 +7,28 @@ export default function Index() {
   const toggleSwitch = () => setIsEnabled(previousState => !previousState);
   const leftEyeText = 'Left Eye';
   const rightEyeText = 'Right Eye';
+  const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginHorizontal: 16,
+  },
+  title: {
+    textAlign: 'center',
+    marginVertical: 8,
+  },
+  button: {
+    justifyContent: 'center',
+    marginTop: 16,
+    fontSize: 28,
+  },
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: '#737373',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+});
+
 
   return (
     <View>
@@ -21,16 +43,27 @@ export default function Index() {
           padding: 10,
         }}
       />
-      <Text style={isEnabled ? { color: '#767577' } : { color: 'black' }}>{leftEyeText}</Text>
-      <Switch
-          ios_backgroundColor="#b0afb0"
-          trackColor={{false: '#b0afb0', true: '#b0afb0'}}
-          thumbColor={isEnabled ? '#564bf5' : '#564bf5'}
-          onValueChange={toggleSwitch}
-          value={isEnabled}
+      <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
+        <Text style={isEnabled ? { color: '#767577', fontSize: 20 } : { color: '#564bf5', fontSize: 20 }}>{leftEyeText}</Text>
+        <Switch
+            ios_backgroundColor="#b0afb0"
+            trackColor={{false: '#b0afb0', true: '#b0afb0'}}
+            thumbColor={isEnabled ? '#564bf5' : '#564bf5'}
+            onValueChange={toggleSwitch}
+            value={isEnabled}
+            style={{marginHorizontal: 10, marginTop: 16}}
+          />
+        <Text style={isEnabled ? { color: '#564bf5', fontSize: 20 } : { color: '#767577', fontSize: 20 }}>{rightEyeText}</Text>
+      </View>
+      <View style={styles.button}>
+        <Button
+          color='#564bf5'
+          title="Start Test"
+          onPress={() => console.log('Button pressed')}
         />
-      <Text style={isEnabled ? { color: 'black' } : { color: '#767577' }}>{rightEyeText}</Text>
+      </View>
    </View>
-   
+
   );
+
 }
