294dbece6c4e7a7bc33ee5258709af3b577735f4
lvanden0 <lily.vandenbergh@icloud.com>
2026-03-11 13:21:58 -0400

    updated index

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,6 @@
 import { useRouter } from "expo-router";
 import { useState } from "react";
-import { Button, StyleSheet, Switch, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { StyleSheet, Switch, Text, TextInput, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
   const [isEnabled, setIsEnabled] = useState(false);
@@ -11,42 +11,23 @@ export default function Index() {
 
   return (
     <View>
-      <TextInput
-        placeholder="User ID"
-        style={{
-          height: 40,
-          width: 200,
-          borderWidth: 1,
-          borderColor: 'gray',
-          margin: 10,
-          padding: 10,
-        }}
-      />
-      <Text style={isEnabled ? { color: '#767577' } : { color: 'black' }}>{leftEyeText}</Text>
-      <Switch
-          ios_backgroundColor="#b0afb0"
-          trackColor={{false: '#b0afb0', true: '#b0afb0'}}
-          thumbColor={isEnabled ? '#564bf5' : '#564bf5'}
-          onValueChange={toggleSwitch}
-          value={isEnabled}
+      <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+        <TextInput
+          placeholder="User ID"
+          style={{
+            height: 40,
+            width: 200,
+            borderWidth: 1,
+            borderColor: 'gray',
+            borderRadius: 8,
+            color: 'black',
+            fontSize: 20,
+            margin: 10,
+            padding: 10,
+            marginTop: 70,
+          }}
         />
-      <Text style={isEnabled ? { color: 'black' } : { color: '#767577' }}>{rightEyeText}</Text>
-
-      {/* Camera Button */}
-      <TouchableOpacity
-        onPress={() => router.push('/camera')}
-        style={{
-          marginTop: 20,
-          marginLeft: 10,
-          backgroundColor: '#564bf5',
-          padding: 12,
-          borderRadius: 8,
-          width: 200,
-          alignItems: 'center',
-        }}
-      >
-        <Text style={{ color: 'white', fontSize: 16 }}>Open Camera</Text>
-      </TouchableOpacity>
+      </View>
 
       <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
         <Text style={isEnabled ? { color: '#767577', fontSize: 20 } : { color: '#564bf5', fontSize: 20 }}>{leftEyeText}</Text>
@@ -61,13 +42,23 @@ export default function Index() {
         <Text style={isEnabled ? { color: '#564bf5', fontSize: 20 } : { color: '#767577', fontSize: 20 }}>{rightEyeText}</Text>
       </View>
 
-      <View style={styles.button}>
-        <Button
-          color='#564bf5'
-          title="Start Test"
-          onPress={() => console.log('Button pressed')}
-        />
+      <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
+        {/* Camera Button */}
+        <TouchableOpacity
+          onPress={() => router.push('/camera')}
+          style={{
+            marginTop: 20,
+            backgroundColor: '#564bf5',
+            padding: 12,
+            borderRadius: 8,
+            width: 200,
+            alignItems: 'center',
+          }}
+        >
+          <Text style={{ color: 'white', fontSize: 20 }}>Open Camera</Text>
+        </TouchableOpacity>
       </View>
+
     </View>
   );
 }
